c3b268639930fa636af4d9f265ab8e78c39d697b
Brspontes <brian.robert16@hotmail.com>
2021-12-02 08:47:25 -0300

    configurando identidade

---
+++ src/Web/NerdStore.Enterprise.Web/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NerdStore.Enterprise.Web.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfig(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login"; //Usuário não logado
+                    options.AccessDeniedPath = "/acesso-negado"; //Usuário não tem permissão
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
