d64efcfb6e3e595173e638db3e07844dc90d3a9e
Brspontes <brian.robert16@hotmail.com>
2022-01-14 10:21:21 -0300

    criado interceptador de request

--- src/Web/NerdStore.Enterprise.Web/Configuration/DependecyInjectionConfig.cs
+++ src/Web/NerdStore.Enterprise.Web/Configuration/DependecyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Enterprise.Web.Extensions;
 using NerdStore.Enterprise.Web.Services;
+using NerdStore.Enterprise.Web.Services.Handlers;
 
 namespace NerdStore.Enterprise.Web.Configuration
 {
@@ -9,8 +10,10 @@ namespace NerdStore.Enterprise.Web.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
         }
---
+++ src/Web/NerdStore.Enterprise.Web/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,39 @@
+﻿using NerdStore.Enterprise.Web.Extensions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Enterprise.Web.Services.Handlers
+{
+    //Interceptador de request para adcionar token
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            this.user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            }
+
+            var token = user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
