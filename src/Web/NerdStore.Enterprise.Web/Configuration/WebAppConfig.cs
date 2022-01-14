e8def2858b3213ce467a74af0752e4847b62afea
Brspontes <brian.robert16@hotmail.com>
2022-01-14 11:40:57 -0300

    configurando cultura

--- src/Web/NerdStore.Enterprise.Web/Configuration/WebAppConfig.cs
+++ src/Web/NerdStore.Enterprise.Web/Configuration/WebAppConfig.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NerdStore.Enterprise.Web.Extensions;
+using System.Globalization;
 
 namespace NerdStore.Enterprise.Web.Configuration
 {
@@ -33,6 +35,14 @@ namespace NerdStore.Enterprise.Web.Configuration
             app.UseRouting();
 
             app.UseIdentityConfiguration();
+
+            var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseMiddleware<ExcepetionMiddleware>();
 
