52dc9021311627adb72b4050e3e489cc7d5df3d5
Brspontes <brian.robert16@hotmail.com>
2022-01-19 10:24:57 -0300

    configuracao incial carrinho

---
+++ src/Services/NerdStore.Enterprise.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Enterprise.WebAPI.Core.Controllers;
+
+namespace NerdStore.Enterprise.Carrinho.API.Controllers
+{
+    public class CarrinhoController : MainController
+    {
+    }
+}
